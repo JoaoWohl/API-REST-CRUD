cfce7a60dbb12fa5b78cac057fd21f73bcd5c022
JoaoWohl <joaovitorsantoswohl9@gmail.com>
2026-04-24 22:13:29 -0300

    feat: Adicionando permissões em rotas

--- src/main/java/com/produto/api/config/security/SecurityConfig.java
+++ src/main/java/com/produto/api/config/security/SecurityConfig.java
@@ -31,6 +31,8 @@ public class SecurityConfig {
                         .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/products").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE,"/products").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
