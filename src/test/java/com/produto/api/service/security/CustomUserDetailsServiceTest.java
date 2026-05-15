30a5b6359354f7c09649e628eab552422ba0278a
JoaoWohl <joaovitorsantoswohl9@gmail.com>
2026-05-15 08:51:20 -0300

    test: adicionando testes para UserDetails

---
+++ src/test/java/com/produto/api/service/security/CustomUserDetailsServiceTest.java
@@ -0,0 +1,47 @@
+package com.produto.api.service.security;
+
+import com.produto.api.entity.user.User;
+import com.produto.api.entity.user.UserRole;
+import com.produto.api.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CustomUserDetailsServiceTest {
+
+    @Mock
+    private UserRepository repository;
+    @InjectMocks
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Test
+    void loadUserByUsername_ShouldReturnSuccess_WhenEverythingOk() {
+        User user = new User("id-1","TestName","TestEmail@test.com","TestPassword", UserRole.USER);
+        String login = "TestEmail@test.com";
+
+        when(repository.findByLogin(login)).thenReturn(Optional.of(user));
+
+        UserDetails result = customUserDetailsService.loadUserByUsername(login);
+
+        assertEquals(user, result);
+    }
+
+    @Test
+    void loadUserByUsername_ShouldReturnFail_WhenUserNotFound() {
+        String login = "TestEmail@test.com";
+
+        when(repository.findByLogin(login)).thenReturn(Optional.empty());
+
+        assertThrows(UsernameNotFoundException.class,() -> customUserDetailsService.loadUserByUsername(login));
+    }
+}
