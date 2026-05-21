4911e3f6bca04edb2738f4e49cae751e5234b2cb
JoaoWohl <joaovitorsantoswohl9@gmail.com>
2026-05-20 22:51:51 -0300

    docs: documentando AuthController no Swagger

--- src/main/java/com/produto/api/controller/AuthController.java
+++ src/main/java/com/produto/api/controller/AuthController.java
@@ -5,6 +5,10 @@ import com.produto.api.dto.request.user.RegisterUserRequestDTO;
 import com.produto.api.dto.response.user.LoginResponseDTO;
 import com.produto.api.dto.response.user.RegisterUserResponseDTO;
 import com.produto.api.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,29 +18,44 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Auth", description = "Endpoints de autenticação")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
     @Autowired
     private AuthService service;
 
+    @Operation(summary = "Faz autenticação do usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário autenticado"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado"),
+    })
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid LoginRequestDTO request) {
         LoginResponseDTO response = service.login(request);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+    @Operation(summary = "Cadastra novo usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuário cadastrado com sucesso"),
+            @ApiResponse(responseCode = "409", description = "Usuário existente"),
+    })
     @PostMapping("/register")
     public ResponseEntity<RegisterUserResponseDTO> register(@RequestBody @Valid RegisterUserRequestDTO request) {
         RegisterUserResponseDTO response = service.register(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @Operation(summary = "Cadastra novo usuário com role específica")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuário cadastrado com sucesso"),
+            @ApiResponse(responseCode = "409", description = "Usuário existente"),
+    })
     @PostMapping("/admin/register")
     public ResponseEntity<RegisterUserResponseDTO> adminRegister(@RequestBody @Valid RegisterUserRequestDTO request) {
         RegisterUserResponseDTO response = service.register(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
-
 
 }
--- src/main/java/com/produto/api/dto/response/user/LoginResponseDTO.java
+++ src/main/java/com/produto/api/dto/response/user/LoginResponseDTO.java
@@ -1,6 +1,9 @@
 package com.produto.api.dto.response.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record LoginResponseDTO(
+        @Schema(example = "ExampleToken12314123")
         String token
 ) {
 }
